5791bdf2dda2025739c0664ebdc7dff6d4a3b153
Christian <christian.etpr10@gmail.com>
2014-07-28 17:58:48 -0700

    Added organization filter

--- app.py
+++ app.py
@@ -18,21 +18,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  org_name = request.args.get('organization_name')
+  return render_template('index.html', org_name=org_name)
 
 @app.route('/find', methods=['POST'])
 def find():
   '''
   Finds issues based on the given label
   '''
+  org_name = request.form.get('org_name', None)
+
   labels = request.form['labels']
   labels.replace(' ', '')
 
-  issues = get('http://localhost:5000/api/issues/labels/'+labels+'?per_page=100')
-  print issues.content
+  if org_name != 'None':
+    issues = get('http://localhost:5000/api/organizations/%s/issues/labels/%s?per_page=100' % (org_name, labels))
+  else:
+    issues = get('http://localhost:5000/api/issues/labels/'+labels+'?per_page=100')
+
   issues = json.loads(issues.content)
 
-  return render_template('index.html', issues=issues['objects'], labels=request.form['labels'])
+  return render_template('index.html', issues=issues['objects'], labels=request.form['labels'], org_name=org_name)
 
 
 if __name__ == "__main__":
