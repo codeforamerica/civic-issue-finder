5d8fc59449cd89c6ff6b661719a47f85b6cf50d2
Christian <christian.etpr10@gmail.com>
2014-07-31 15:26:08 -0700

    Made the embedable code in index.html dynamic

---
+++ static/main.js
@@ -0,0 +1,23 @@
+function update_snippet () {
+  var labels = document.getElementById('labels').value;
+  var org = document.getElementById('organization').value;
+  var code = document.getElementById('embed');
+  if (org !== '') {
+    if (labels !== '') {
+      embed_string = '&lt;iframe src="http://localhost:5000/widget?organization_name='+ encodeURIComponent(org) +'&default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+    } else{
+      embed_string = '&lt;iframe src="http://localhost:5000/widget?organization_name='+ encodeURIComponent(org) +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+    };
+  } else if (labels !== '') {
+    embed_string = '&lt;iframe src="http://localhost:5000/widget?default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+  };
+  code.innerHTML = embed_string;
+}
+
+var labels = document.getElementById('labels');
+var org = document.getElementById('organization');
+
+org.addEventListener('change', update_snippet);
+labels.addEventListener('keyup', update_snippet);
+
+console.log('holaa');
