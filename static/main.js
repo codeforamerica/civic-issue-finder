c2ef9f093c65c7b8dfe46afc7a5b355d6d6d7801
Christian <christian.etpr10@gmail.com>
2014-07-31 16:24:26 -0700

    More hot-fixes

--- static/main.js
+++ static/main.js
@@ -4,12 +4,12 @@ function update_snippet () {
   var code = document.getElementById('embed');
   if (org !== '') {
     if (labels !== '') {
-      embed_string = '&lt;iframe src="http://localhost:5000/widget?organization_name='+ encodeURIComponent(org) +'&default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'&default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
     } else{
-      embed_string = '&lt;iframe src="http://localhost:5000/widget?organization_name='+ encodeURIComponent(org) +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'" width="300" height="600"&gt; &lt;/iframe&gt;';
     };
   } else if (labels !== '') {
-    embed_string = '&lt;iframe src="http://localhost:5000/widget?default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+    embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
   };
   code.innerHTML = embed_string;
 }
