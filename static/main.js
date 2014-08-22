eae5486f0b0bbb4aeb0572ee3e2e307af99390f6
Andrew Hyder <hyd415@gmail.com>
2014-08-22 14:13:00 -0700

    Rewrote how main.js build the wdiget string.

--- static/main.js
+++ static/main.js
@@ -1,27 +1,36 @@
-function update_snippet () {
+function updateSnippet () {
   var labels = document.getElementById('labels').value;
   var org = document.getElementById('organization').value;
   var code = document.getElementById('embed');
   var widget = document.getElementById('widget');
+  var number = document.getElementById('number');
 
-  if (org !== '') {
-    if (labels !== '') {
-      embed_string = '&lt;iframe src="http://codeforamerica.org/geeks/civicissues/widget?organization_name='+ encodeURIComponent(org) +'&labels='+ labels.trim() +'" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
-      widget.src = 'http://codeforamerica.org/geeks/civicissues/widget?organization_name='+ encodeURIComponent(org) +'&labels='+ labels.trim();
-    } else{
-      embed_string = '&lt;iframe src="http://codeforamerica.org/geeks/civicissues/widget?organization_name='+ encodeURIComponent(org) +'" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
-      widget.src = 'http://codeforamerica.org/geeks/civicissues/widget?organization_name='+ encodeURIComponent(org);
-    };
-  } else if (labels !== '') {
-    embed_string = '&lt;iframe src="http://codeforamerica.org/geeks/civicissues/widget?labels='+ labels.trim() +'" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
-    widget.src = 'http://codeforamerica.org/geeks/civicissues/widget?labels='+ labels.trim();
-  } else {
-    embed_string = '&lt;iframe src="http://codeforamerica.org/geeks/civicissues/widget" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
-    widget.src = 'http://codeforamerica.org/geeks/civicissues/widget';
-  };
-  code.innerHTML = embed_string;
+  var embedString = 'http://codeforamerica.org/geeks/civicissues/widget';
+  if (org || labels || number){
+    embedString += '?'
+  }
+  if (org) {
+    embedString += 'organization_name=' + encodeURIComponent(org);
+    if (labels || number){
+      embedString += '&';
+    }
+  }
+  if (labels) {
+    embedString += 'labels=' + labels.trim();
+    if (number) {
+      embedString += '&';
+    }
+  }
+  if (number) {
+    embedString += 'number=' + number.value;
+  }
 
+  // Reload the iframe
+  widget.src = embedString;
 
+  // Finish the embedString for the form
+  embedString = '&lt;iframe src="' + embedString + '" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
+  code.innerHTML = embedString;
 }
 
 window.onload = function() {
