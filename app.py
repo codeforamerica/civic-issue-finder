ea023235e28123896442d56423a2b152d5865ce9
Andrew Hyder <hyd415@gmail.com>
2014-08-22 12:14:43 -0700

    Static path

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from requests.exceptions import ConnectionError
 # Init
 # -------------------
 
-app = Flask(__name__)
+app = Flask(__name__,  static_folder='static', static_url_path='/geeks/civicissues/static')
 
 # -------------------
 # Routes
