5d75a17ed5f92421d1235fea25eb0fd00f88eae2
Andrew Hyder <hyd415@gmail.com>
2014-08-22 14:14:19 -0700

    Rewrote how app.py writes its api request.

--- app.py
+++ app.py
@@ -46,47 +46,47 @@ def embed():
 
 @app.route('/geeks/civicissues/widget')
 def widget():
-  '''
-  Finds issues based on the given label. Render them in the widget
-  '''
-  # Get optional parameters
-  labels = request.args.get('labels', None)
-  org_name = request.args.get('organization_name')
+    '''
+    Finds issues based on the given label. Render them in the widget
+    '''
+    # Get optional parameters
+    labels = request.args.get('labels', None)
+    org_name = request.args.get('organization_name')
+    number = request.args.get('number')
 
-  # Get the actual issues from the API
-  try:
-    # If we have an organization name only query that organization
-    if org_name != None:
-      if labels != None:
-        issues = get('http://codeforamerica.org/api/organizations/%s/issues/labels/%s' % (org_name, labels))
-      else:
-        issues = get('http://codeforamerica.org/api/organizations/%s/issues' % org_name)
-    # Otherwise get issues across all organizations
-    else:
-      if labels != None:
-        issues = get('http://codeforamerica.org/api/issues/labels/%s' % labels)
-      else:
-        issues = get('http://codeforamerica.org/api/issues')
-  except ConnectionError, e:
-    return render_template('widget.html', error=True)
+    # Build the url
+    issues_url = 'http://codeforamerica.org/api/'
+    if org_name:
+        issues_url += 'organizations/%s/' % org_name
+    issues_url += 'issues'
+    if labels:
+        issues_url += '/labels/%s' % labels
+    if number:
+        issues_url += '?per_page=%s' % number
 
-  if issues.status_code != 200:
-    return render_template('widget.html', error=True)
+    # Get the actual issues from the API
+    try:
+        issues_response = get(issues_url)
+    except ConnectionError, e:
+        return render_template('widget.html', error=True)
 
-  # Parse the API response
-  issues = issues.json()
-  issues = issues['objects']
+    if issues_response.status_code != 200:
+        return render_template('widget.html', error=True)
 
-  # Format each issue
-  for issue in issues:
-    # Add text_color to labels to make them more readable
-    for label in issue['labels']:
-      if label['color'] < '888888':
-        label['text_color'] = 'FFFFFF'
-      else:
-        label['text_color'] = '000000'
+    # Parse the API response
+    issues_json = issues_response.json()
+    issues = issues_json['objects']
 
-  return render_template('widget.html', issues=issues, labels=labels)
+    # Format each issue
+    for issue in issues:
+        # Add text_color to labels to make them more readable
+        for label in issue['labels']:
+            if label['color'] < '888888':
+                label['text_color'] = 'FFFFFF'
+            else:
+                label['text_color'] = '000000'
+
+    return render_template('widget.html', issues=issues, labels=labels)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
