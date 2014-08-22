093b90051743fed78088533a872c430a4b74c3da
Andrew Hyder <hyd415@gmail.com>
2014-08-22 00:37:19 -0700

    Made issues embed pretty. Simplified tool.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 # Imports
 # -------------------
 
-from flask import Flask, render_template, request, session, Markup, redirect, url_for
-import json, markdown
+from flask import Flask, render_template, request, redirect, url_for
+import json
 from requests import get
 from requests.exceptions import ConnectionError
 
@@ -12,7 +12,6 @@ from requests.exceptions import ConnectionError
 # -------------------
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '\xbd[;\xfeZs$\x94\x13J\x8fq\xe8\xbd\x9e\xc0\xe1\xa9"\xbe-\xb0\x80\xaa'
 
 # -------------------
 # Routes
@@ -20,63 +19,54 @@ app.config['SECRET_KEY'] = '\xbd[;\xfeZs$\x94\x13J\x8fq\xe8\xbd\x9e\xc0\xe1\xa9"
 
 @app.route('/')
 def index():
+  return render_template('index.html')
 
-  if not session.get('organizations', None):
-    names = []
+
+@app.route('/embed')
+def embed():
+    '''
+    Show an editable embed form
+    '''
+
     # Get all of the organizations from the api
     organizations = get('http://codeforamerica.org/api/organizations?per_page=200')
-    organizations = json.loads(organizations.content)
+    organizations = organizations.json()
 
     # Filter out just the organization names
+    names = []
     for org in organizations['objects']:
-      names.append(org['name'])
+        names.append(org['name'])
 
     # Alphabetize names
     names.sort()
 
-    session['organizations'] = names
-    session.modified = True
+    # Render index and pass in all of the organization names
+    return render_template('embed.html', organization_names=names)
 
-  # Render index and pass in all of the organization names
-  return render_template('index.html', organization_names=session['organizations'])
 
 @app.route('/widget')
 def widget():
   '''
-  Render the basic empty widget
-  '''
-  session['org_name'] = request.args.get('organization_name')
-  default_labels = request.args.get('default_labels')
-  return redirect(url_for('find', labels=default_labels))
-
-@app.route('/find', methods=['GET', 'POST'])
-def find():
-  '''
   Finds issues based on the given label. Render them in the widget
   '''
-  if request.method == 'GET':
-    labels = request.args.get('labels', None)
-  elif request.method == 'POST':
-    # Get labels from form
-    labels = request.form['labels']
-
   # Get optional parameters
-  org_name = session.get('org_name', None)
+  labels = request.args.get('labels', None)
+  org_name = request.args.get('organization_name')
 
   # Get the actual issues from the API
   try:
     # If we have an organization name only query that organization
     if org_name != None:
       if labels != None:
-        issues = get('http://codeforamerica.org/api/organizations/%s/issues/labels/%s?per_page=100' % (org_name, labels))
+        issues = get('http://codeforamerica.org/api/organizations/%s/issues/labels/%s' % (org_name, labels))
       else:
-        issues = get('http://codeforamerica.org/api/organizations/%s/issues?per_page=100' % org_name)
+        issues = get('http://codeforamerica.org/api/organizations/%s/issues' % org_name)
     # Otherwise get issues across all organizations
     else:
       if labels != None:
-        issues = get('http://codeforamerica.org/api/issues/labels/%s?per_page=100' % labels)
+        issues = get('http://codeforamerica.org/api/issues/labels/%s' % labels)
       else:
-        issues = get('http://codeforamerica.org/api/issues?per_page=100')
+        issues = get('http://codeforamerica.org/api/issues')
   except ConnectionError, e:
     return render_template('widget.html', error=True)
 
@@ -84,21 +74,19 @@ def find():
     return render_template('widget.html', error=True)
 
   # Parse the API response
-  issues = json.loads(issues.content)  
+  issues = issues.json()
+  issues = issues['objects']
 
   # Format each issue
-  for iss in issues['objects']:
-    # Parse the issue body from markdown to html
-    if iss['body']:
-      iss['body'] = Markup(markdown.markdown(iss['body']))
+  for issue in issues:
     # Add text_color to labels to make them more readable
-    for l in iss['labels']:
-      if l['color'] < '888888':
-        l['text_color'] = 'FFFFFF'
+    for label in issue['labels']:
+      if label['color'] < '888888':
+        label['text_color'] = 'FFFFFF'
       else:
-        l['text_color'] = '000000'
+        label['text_color'] = '000000'
 
-  return render_template('widget.html', issues=issues['objects'], labels=labels)
+  return render_template('widget.html', issues=issues, labels=labels)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -2,18 +2,26 @@ function update_snippet () {
   var labels = document.getElementById('labels').value;
   var org = document.getElementById('organization').value;
   var code = document.getElementById('embed');
+  var widget = document.getElementById('widget');
+
   if (org !== '') {
     if (labels !== '') {
-      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'&default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'&labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+      widget.src = 'http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'&labels='+ labels.trim();
     } else{
       embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+      widget.src = 'http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org);
     };
   } else if (labels !== '') {
-    embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?default_labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+    embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+    widget.src = 'http://civicissues.herokuapp.com/widget?labels='+ labels.trim();
   } else {
     embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget" width="300" height="600"&gt; &lt;/iframe&gt;';
+    widget.src = 'http://civicissues.herokuapp.com/widget';
   };
   code.innerHTML = embed_string;
+
+
 }
 
 var labels = document.getElementById('labels');
