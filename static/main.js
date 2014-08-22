fe8c962e18d4606ee070b5da655c7381f3c8d6da
Andrew Hyder <hyd415@gmail.com>
2014-08-22 00:46:40 -0700

    fixed embed code

--- static/main.js
+++ static/main.js
@@ -6,17 +6,17 @@ function update_snippet () {
 
   if (org !== '') {
     if (labels !== '') {
-      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'&labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'&labels='+ labels.trim() +'" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
       widget.src = 'http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'&labels='+ labels.trim();
     } else{
-      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+      embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org) +'" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
       widget.src = 'http://civicissues.herokuapp.com/widget?organization_name='+ encodeURIComponent(org);
     };
   } else if (labels !== '') {
-    embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?labels='+ labels.trim() +'" width="300" height="600"&gt; &lt;/iframe&gt;';
+    embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget?labels='+ labels.trim() +'" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
     widget.src = 'http://civicissues.herokuapp.com/widget?labels='+ labels.trim();
   } else {
-    embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget" width="300" height="600"&gt; &lt;/iframe&gt;';
+    embed_string = '&lt;iframe src="http://civicissues.herokuapp.com/widget" width="100%" height="600" frameBorder="0"&gt; &lt;/iframe&gt;';
     widget.src = 'http://civicissues.herokuapp.com/widget';
   };
   code.innerHTML = embed_string;
