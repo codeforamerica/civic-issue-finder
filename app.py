b7098622b28d8b943db2fad6d222abeb2eb00e00
Andrew Hyder <hyd415@gmail.com>
2014-11-09 13:31:26 -0800

    Faster cfapi issues request.

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def engine_light():
         status = "GitHub Avatars not loading."
 
     # Check if CfAPI is up.
-    response = get("http://codeforamerica.org/api/issues")
+    response = get("http://codeforamerica.org/api/issues?per_page=1")
     if response.status_code != 200:
         status = "CfAPI not returning Issues."
 
