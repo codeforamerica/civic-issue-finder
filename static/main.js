d6f2569bba7c447b9c270bccf2c607f951d33043
Stefan Bohacek <stefan.bohacek@gmail.com>
2017-10-04 09:13:02 -0400

    Fixed iframe resizing.

--- static/main.js
+++ static/main.js
@@ -1,5 +1,16 @@
+// Function to retrieve document height
+function getDocumentHeight(doc) {
+  var doc = doc || document;
+  var body = doc.body, html = doc.documentElement;
+  var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  return height;
+}
 // Resize iframe to fit content
 function resize() {
-  var newheight = document.getElementById("widget").contentWindow.document.body.scrollHeight;
-  document.getElementById("widget").height = newheight + "px";
+  var ifrm = document.getElementById('widget');
+  var doc = ifrm.contentDocument ? ifrm.contentDocument : ifrm.contentWindow.document;
+  ifrm.style.visibility = 'hidden';
+  ifrm.style.height = "10px";
+  ifrm.style.height = getDocumentHeight(doc) + 4 + "px";
+  ifrm.style.visibility = 'visible';
 }
